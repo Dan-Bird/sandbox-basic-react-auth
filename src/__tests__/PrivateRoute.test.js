d2485fcbf6be447bb23bb5f69217ba159511c622
Daniel <info@daniel-bird.co.uk>
2020-11-25 22:33:26 +0000

    Test PrivateRouter

---
+++ src/__tests__/PrivateRoute.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PrivateRoute from '../components/PrivateRoute';
+
+describe('The PrivateRout component', () => {
+  test('should render the component passed down to it', () => {
+    const FakeComponent = () => <h1>Fake Component</h1>;
+
+    window.history.pushState({}, 'Test', '/test');
+    render(
+      <Router>
+        <PrivateRoute path="/test" component={FakeComponent} />
+      </Router>
+    );
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/Fake Component/i);
+  });
+});
