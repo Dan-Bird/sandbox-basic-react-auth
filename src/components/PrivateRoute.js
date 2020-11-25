4a1b67e809b916edfa0ad162113226ecf3c28b5a
Daniel <info@daniel-bird.co.uk>
2020-11-25 22:07:17 +0000

    Create PrivateRoute

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return <Route {...rest} render={props => <Component {...props} />} />;
+};
+
+export default PrivateRoute;
