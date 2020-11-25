c501c5b89d2792a9fb41bbf182f1048dba771e8b
Daniel <info@daniel-bird.co.uk>
2020-11-25 18:22:14 +0000

    Render Admin component on "/admin" route

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home screen on "/" route', () => {
+  window.history.pushState({}, 'Test', '/');
+
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+
+  expect(screen.getByRole('heading')).toHaveTextContent(/home screen/i);
+});
+
+test('renders admin screen on "/admin" route', () => {
+  window.history.pushState({}, 'Test', '/admin');
+
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+
+  expect(screen.getByRole('heading')).toHaveTextContent(/admin screen/i);
 });
