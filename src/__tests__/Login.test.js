b36cbd93a191405e49d8ff2bca6ed3ab4a9c96df
Daniel <info@daniel-bird.co.uk>
2020-12-07 15:37:26 +0000

    Remove unused import

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -1,12 +1,10 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Login from '../components/Login';
 
-jest.mock('../components/SignUp', () => () => <h1>Test SignUp</h1>);
-
 describe('Login component', () => {
-  it('should link through to the SignUp page', async () => {
+  it('should link through to the SignUp page', () => {
     render(
       <Router>
         <Login />
